6ec79573cccbce01d6dd310bf4ba0c6f308850b6
woobin <woobin0413@gmail.com>
2018-09-12 21:02:35 -0500

    .
    .

--- routes/topic.js
+++ routes/topic.js
@@ -3,26 +3,22 @@ module.exports = function(){
   var router = require('express').Router();
   //하나의 파일을 만들기 위한 form tag
   router.get('/', function(req,res){
-    res.render('view');
-  });
-
-  router.get('/login', function(req,res){
     res.render('auth/login');
   });
-
-//HTML 로 보낼시 (bootstrap 이용시)
-  router.get('/welcome', (req, res) => {
-   res.sendFile('welcome.html', {
-     root: './views/auth/'
-   });
-});
 
   router.post('/auth/login', function(req,res){
     var username = req.body.username;
     if(username) {
       res.redirect('/welcome');
     }
-
   });
+
+  //HTML 로 보낼시 (bootstrap 이용시)
+  router.get('/welcome', (req, res) => {
+   res.sendFile('welcome.html', {
+     root: './views/auth/'
+   });
+});
+
     return router;
 }
