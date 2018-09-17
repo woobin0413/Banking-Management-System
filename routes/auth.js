d1879553f56aa8b57801515e14dd2cc9ccac0814
woobin <woobin0413@gmail.com>
2018-09-16 20:24:26 -0500

    hex
    update

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,9 @@ module.exports = function(passport){
 
   var router = require('express').Router();
   var conn = require('../config/config');
-  var bkfd2Password = require("pbkdf2-password");
-  var hasher = bkfd2Password();
+  const crypto = require('crypto');
+  const secret = 'jpfaojdf3$@#$@#523efj2';
+
   //하나의 파일을 만들기 위한 form tag
   router.get('/', function(req,res){
     res.render('auth/login');
@@ -11,8 +12,15 @@ module.exports = function(passport){
 
   router.post('/auth/login', function(req,res){
     var username = req.body.username;
+    var password = req.body.password;
+    var hash = crypto.createHmac('sha256', secret)
+                     .update(password)
+                     .digest('hex');
     if(username) {
       res.redirect('/welcome');
+      if(password) {
+        console.log(hash);
+      }
     }
   });
 
