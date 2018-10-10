dbb71e7d3d6114716fe352ae7101f11af66944da
woobin <woobin0413@gmail.com>
2018-10-09 20:56:30 -0500

    .
    .

--- routes/topic.js
+++ routes/topic.js
@@ -2,20 +2,33 @@ module.exports = function(){
 
   var router = require('express').Router();
   var config = require('../config/config.json');
-  var multer  = require('multer')
-  var upload = multer({ dest: 'uploads/' })
+  var multer = require('multer'); // express에 multer모듈 적용 (for 파일업로드)
+  var storage = multer.diskStorage({
+  //upload 할떄마다 이름이 랜덤으로 설정되있는데 그것을 원래이름이나 위치를 다시 변경시 필요사항
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/') // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname) // cb 콜백함수를 통해 전송된 파일 이름 설정
+  }
+});
+
+var upload = multer({ storage: storage })
+
 
   const request = require('request');
-  const apiKey = "74be192b0326b5dc2bb04884ab39d5e4";
+  // const apiKey = "74be192b0326b5dc2bb04884ab39d5e4";
   let t1,t2,t3,l1,l2,l3;
 
 //celcious=> units=metric
  router.get('/homepage', function(req,res){
    res.render('topic/homepage');
  });
- router.post('/upload', function(req, res){
-  res.send('업로드 성공!');
+ router.post('/upload', upload.single('userfile'), function(req, res){
+   res.send('Uploaded! : '+req.file); // object를 리턴함
+   console.log(req.file); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
 });
+
    // req.file 은 `avatar` 라는 필드의 파일 정보입니다.
   // 텍스트 필드가 있는 경우, req.body가 이를 포함할 것입니다.
 
@@ -26,7 +39,7 @@ module.exports = function(){
 
  router.get('/weather', function(req,res){
 
-   let url = `http://api.openweathermap.org/data/2.5/group?id=5106834,5100399,4076784&units=imperial&appid=${apiKey}`
+   let url = `http://api.openweathermap.org/data/2.5/group?id=5106834,5100399,4076784&units=imperial&appid=${config.apiKey}`
 
     request(url, function (err, response, body) {
      let weather = JSON.parse(body)
@@ -52,12 +65,13 @@ module.exports = function(){
 
     res.render('topic/weather',{t1: t1, t2: t2,t3:t3,l1:l1,l2:l2,l3:l3});
      });
+
 });
 //
 //img 형식으로 날씨및 정보나올수잇게
 //http://api.openweathermap.org/data/2.5/weather?zip=36116&mode=html&units=imperial&appid=74be192b0326b5dc2bb04884ab39d5e4
 router.post('/weather', function(req,res){
-  let url = `http://api.openweathermap.org/data/2.5/weather?zip=${req.body.zipcode}&units=imperial&appid=${apiKey}`
+  let url = `http://api.openweathermap.org/data/2.5/weather?zip=${req.body.zipcode}&units=imperial&appid=${config.apiKey}`
   request(url, function (err, response, body) {
 
      if(err){
