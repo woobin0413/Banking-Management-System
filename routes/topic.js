2de89c4ce514db0ace15149d32944417f606ff8d
woobin <woobin0413@gmail.com>
2018-10-09 21:08:13 -0500

    MULTER ADVANCED
    ..

--- routes/topic.js
+++ routes/topic.js
@@ -23,7 +23,11 @@ module.exports = function(){
     res.render('topic/homepage');
   });
   router.post('/upload', upload.single('userfile'), function(req, res){
-    res.send('Uploaded! : '+req.file); // object를 리턴함
+    if(req.file) {
+      res.render('topic/homepage', {fileResult: req.file.filename})
+    } else {
+      res.render('topic/homepage', {fileResult: `NO ATTACHMENT FOUND!!`});
+    }
     console.log(req.file); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
   });
 
